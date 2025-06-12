e2f4d3889f0b124affae4bf05ef1dc5d7851a000
kasun prabath <kasunp@Standards-Mac-mini.local>
2025-06-12 14:31:23 +0530

    added TS decleration for sound plaer and pdf viewer

--- src/global.d.ts
+++ src/global.d.ts
@@ -8,5 +8,5 @@ declare module 'react-native/Libraries/Blob/Blob' {
   export default Blob
 }
 
-declare module "react-native-pdf" {}
-declare module "react-native-sound-player" {}
+declare module "react-native-pdf"
+declare module "react-native-sound-player"
